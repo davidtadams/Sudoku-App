31e6b6ae711adb5ff68e9c71e26ea0c03ca0c14e
davidtadams <adams.t.david@gmail.com>
2015-11-16 16:50:44 -0700

    got numbers on the board working

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
   var canvas = document.getElementById("canvas");
   var ctx = canvas.getContext("2d");
 
+  /* Drawing the grid system for Sudoku game board */
   ctx.beginPath();
   for (var i = 0; i < 10; i++) {
     if (i != 0 && i != 3 && i != 6 && i != 9) {
@@ -42,8 +43,16 @@ $(document).ready(function() {
     ctx.closePath();
   }
 
-
-
-
+  ctx.font = "50px monospace";
+  ctx.textBaseline = "hanging";
+  ctx.fillText('1', 15, 10);
+  ctx.fillText('2', 75, 10);
+  ctx.fillText('3', 135, 10);
+  ctx.fillText('4', 15, 70);
+  ctx.fillText('5', 75, 70);
+  ctx.fillText('6', 135, 70);
+  ctx.fillText('7', 15, 130);
+  ctx.fillText('8', 75, 130);
+  ctx.fillText('9', 135, 130);
 
 });
