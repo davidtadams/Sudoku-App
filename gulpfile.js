a90b7b3ad5a391efd71631044a9c161d2a8b2a88
davidtadams <adams.t.david@gmail.com>
2015-11-18 11:24:18 -0700

    changed gulp to reflect new file additions with foundation

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,53 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
-var rename = require('gulp-rename');
 var browserSync = require('browser-sync').create();
 var reload = browserSync.reload;
 
 /* all gulp tasks to build production code */
 gulp.task('build-moveHTML', function() {
   gulp
-    .src('./*.html')
-    .pipe(gulp.dest('./build'));
+    .src('*.html')
+    .pipe(gulp.dest('build'));
 });
 
 gulp.task('build-moveCSS', function() {
   gulp
-    .src('./css/*.css')
-    .pipe(gulp.dest('./build'));
+    .src('css/*.css')
+    .pipe(gulp.dest('build/css'));
+});
+
+gulp.task('build-moveJS', function() {
+  gulp
+    .src(['js/modernizr.js', 'js/foundation.min.js'])
+    .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('build-cat-minify-jquery-js', function() {
-  return gulp.src(['./scripts/jquery-1.4.2.js', './scripts/jquery.hotkeys.js'])
+  return gulp.src(['js/jquery.js', 'js/jquery.hotkeys.js'])
     .pipe(concat('jquery.min.js'))
-    .pipe(gulp.dest('./scripts'))
+    .pipe(gulp.dest('js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./scripts'))
-    .pipe(gulp.dest('./build/scripts'));
+    .pipe(gulp.dest('js'))
+    .pipe(gulp.dest('build/js'));
 });
 
 gulp.task('build-cat-minify-game-js', function() {
-  return gulp.src(['./scripts/SudokuGame.js', './scripts/game.js'])
+  return gulp.src(['js/SudokuGame.js', 'js/game.js'])
     .pipe(concat('game.min.js'))
-    .pipe(gulp.dest('./scripts'))
+    .pipe(gulp.dest('js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./scripts'))
-    .pipe(gulp.dest('./build/scripts'));
+    .pipe(gulp.dest('js'))
+    .pipe(gulp.dest('build/js'));
 });
 
 /* all gulp tasks to make a automated dev enviroment */
 gulp.task('dev-cat-min-js', function() {
-  return gulp.src(['./scripts/SudokuGame.js', './scripts/game.js'])
+  return gulp.src(['js/SudokuGame.js', 'js/game.js'])
     .pipe(concat('game.min.js'))
-    .pipe(gulp.dest('./scripts'))
+    .pipe(gulp.dest('js'))
     .pipe(uglify())
-    .pipe(gulp.dest('./scripts'))
+    .pipe(gulp.dest('js'))
     .pipe(reload({stream: true}));
 })
 
@@ -56,11 +61,11 @@ gulp.task('dev-serve', ['dev-cat-min-js'], function() {
   });
 
   gulp.watch("*.html").on("change", reload);
-  gulp.watch("*.css").on("change", reload);
-  gulp.watch("scripts/*.js", ['dev-js-watch']);
+  gulp.watch("css/*.css").on("change", reload);
+  gulp.watch("js/*.js", ['dev-js-watch']);
 })
 
 /* gulp cli runners */
 gulp.task('default', ['build']);
-gulp.task('build', ['build-moveHTML', 'build-moveCSS', 'build-cat-minify-jquery-js', 'build-cat-minify-game-js']);
+gulp.task('build', ['build-moveHTML', 'build-moveCSS', 'build-moveJS', 'build-cat-minify-jquery-js', 'build-cat-minify-game-js']);
 gulp.task('dev', ['dev-serve']);
